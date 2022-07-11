e42ec9365f149fd002b2349055f215d64aaea63e
cwill83247 <breakthrumusic@hotmail.com>
2022-07-11 14:10:47 +0000

    adding function placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("testing, testing 123");
+console.log("testing, testing 123")
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+

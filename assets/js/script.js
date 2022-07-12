2aeec35a2bac090e5ca89192c1a5b1f3366ba3ed
cwill83247 <breakthrumusic@hotmail.com>
2022-07-12 15:33:05 +0000

    adding JavaScript for Additon

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * script called when game is first loaded.. the function can then be hovered over when in script and will show this info 
  */
 
-function runGame() {
+function runGame(gameType) {
         //creates 2 random whole numbers between 1 and 25 and assign them to num1 and num 2
     let num1 = Math.floor(Math.random () * 25)+1 ;
     let num2 = Math.floor(Math.random () * 25)+1 ;
+
+    if (gameType == "addition") {
+       displayAdditionQuestion(num1, num2);
+    } else {
+        alert('unknown game type: ${gameType');
+        throw 'unknown game type: ${gameType} Aborting!' ;
+    }
 
 }
 runGame();
@@ -44,9 +51,12 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {    //adding arguments 
+    document.getElementById('operand1').textContent = operand1;        // this is grabbing whatever random value we get and putting it into the HTML page
+    document.getElementById('operand2').textContent = operand2;       // unsure exactly which operand is doing which or were value is coming from.. REVISIT
+    document.getElementById('operator').textContent = "+";          // sp we are setting operating to PLUS
 }
+
 
 function displaySubtractQuestion() {
 

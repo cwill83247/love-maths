afb2e2db29508caaedbd70c366fa88a7d517c2bf
cwill83247 <breakthrumusic@hotmail.com>
2022-07-13 08:52:47 +0000

    updated to get additon section working still no joy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,43 @@
-console.log("testing, testing 123");
+// mine failed initally added notes to show why as useful reference //
+//Added a Correct when correct / and added Ammendments bit to show what i got wrong 
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function() {    //CORRECT
+    let buttons = document.getElementsByTagName("button");     //CORRECT
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                runGame("gameType");
-            }
-        });
-    }
+    for (let button of buttons) {                              //CORRECT  used instead of a for loop this is more modern
+        button.addEventListener("click", function() {           //CORRECT
+            if (this.getAttribute("data-type") === "submit") {    //CORRECT
+                alert("You clicked Submit!");                      //CORRECT
+            } else {								//CORRECT
+                let gameType = this.getAttribute("data-type");       //CORRECT
+                runGame("gameType");                                     //CORRECT
+            }               
+        });                                                              //CORRECT
+    }                                                                      //CORRECT
 
-runGame("addition");
+runGame("addition");                                                  //CORRECT
 
-});
+});                                                                    //CORRECT
 
 /**
  * the main game loop we are using a DOCSTRING here 
  * script called when game is first loaded.. the function can then be hovered over when in script and will show this info 
  */
 
-function runGame(gameType) {
+function runGame(gameType) {                                   //CORRECT
         //creates 2 random whole numbers between 1 and 25 and assign them to num1 and num 2
-    let num1 = Math.floor(Math.random () * 25)+1 ;
-    let num2 = Math.floor(Math.random () * 25)+1 ;
+    let num1 = Math.floor(Math.random () * 25)+1 ;                 //CORRECT
+    let num2 = Math.floor(Math.random () * 25)+1 ;                  //CORRECT
 
-    if (gameType === "addition") {
-       displayAdditionQuestion (num1, num2);
+    if (gameType === "addition") {                                             //CORRECT
+       displayAdditionQuestion (num1, num2);                                    //CORRECT   
     } else {
-        alert(`unknown game type: ${gameType}`);
-        throw `unknown game type: ${gameType}. Aborting!` ;
-    }
+        alert(`unknown game type: ${gameType}`);                                 //CORRECT
+        throw `unknown game type: ${gameType}. Aborting!`;                      //CORRECT
+    }                                                                               //CORRECT
 
-}
-runGame();
+}                                                                                 //CORRECT
+//     this was here ????    runGame();
 
 function checkAnswer() {
 
@@ -54,7 +55,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(operand1, operand2) {    //adding arguments 
+function displayAdditionQuestion(operand1, operand2) {    //adding arguments     //CORRECT
     document.getElementById('operand1').textContent = operand1;        // this is grabbing whatever random value we get and putting it into the HTML page
     document.getElementById('operand2').textContent = operand2;       // unsure exactly which operand is doing which or were value is coming from.. REVISIT
     document.getElementById('operator').textContent = "+";          // sp we are setting operating to PLUS
@@ -68,5 +69,3 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
 
 }
-
-
